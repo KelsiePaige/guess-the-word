4ed323524485d20d22d8068b4c8cc6858bd5e24d
Kelsie B <klsbrwn94@gmail.com>
2023-03-28 20:56:44 -0400

    Added global variables

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// The unordered list where the player's guessedletters will appear.
+const guessedLetters = document.querySelector(".guessed-letters");
+// The button with the text "Guess!" in it.
+const guess = document.querySelector(".guess");
+// The text input where the player will guess a letter.
+const letter = document.querySelector(".letter");
+// The empty paragraph where the word in progress will apppear.
+const wordInProgress = document.querySelector(".word-in-progress");
+// The paragraph where the remaining guesses will display.
+const remainingGuesses = document.querySelector(".remaining");
+// The span inside the paragraph where the remaining guesses will display.
+const guessCount = document.querySelector("span");
+// The empty paragraph where messages will appear when the player guesses a letter.
+const message = document.querySelector(".message");
+// The hidden button that will appear prompting the player to play again.
+const button = document.querySelector(".play-again");
+// Starting word for testing
+const word = "magnolia";
+
+//console.log(guessedLetters, guess, letter, wordInProgress, remainingGuesses, guessCount, message, button, word);
